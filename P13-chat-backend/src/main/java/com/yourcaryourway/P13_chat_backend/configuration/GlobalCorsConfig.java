a803fd33b10145cbd4436d9932558633ad1d3404
Myriam Denizot <denizot.myriam@gmail.com>
2025-01-24 18:42:30 +0100

    config for add vps private

--- P13-chat-backend/src/main/java/com/yourcaryourway/P13_chat_backend/configuration/GlobalCorsConfig.java
+++ P13-chat-backend/src/main/java/com/yourcaryourway/P13_chat_backend/configuration/GlobalCorsConfig.java
@@ -14,7 +14,7 @@ public class GlobalCorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:4200")
+                        .allowedOrigins("http://localhost:4200","http://82.67.42.254:4200")
                         .allowedMethods("*")
                         .allowedHeaders("*")
                         .allowCredentials(true);
